4b5b9f09bc22c2551e509611e966147261c7cdfc
dbjdbj <dbj@dbj.org>
2019-12-27 11:05:12 +0100

    allocators moved to nanolib

--- allocators/allocators_testing.cpp
+++ allocators/allocators_testing.cpp
@@ -1,32 +1,10 @@
-#include "stack_allocator.h"
-#include "aligned_allocator.h"
+#include "../common.h"
+#include "../dbj--nanolib/nonstd/aligned_allocator.h"
+#include "../dbj--nanolib/nonstd/stack_allocator.h"
 
-//std::size_t memory = 0;
-//std::size_t alloc = 0;
-//
-//void* operator new(std::size_t s) // throw(std::bad_alloc)
-//{
-//    memory += s;
-//    ++alloc;
-//    return malloc(s);
-//}
-//
-//void  operator delete(void* p) throw()
-//{
-//    --alloc;
-//    free(p);
-//}
-
-// very fast and very dangerous
-template< template< class T_, class A_> typename container_type, typename value_type, size_t stack_alloc_size = 0xFFF, 
-    typename stack_alloc_type  =  stackalloc_space::stack_allocator<value_type, stack_alloc_size> >
-using small_container = container_type< value_type , stack_alloc_type > ;
-
-static auto dumsy = []
-    {
-        using small_vector = small_container< std::vector, char > ;
-        //using small_vector = std::vector<char> ;
-
+TU_REGISTER([]
+{
+        using small_vector = dbj::nanolib::alloc::small_container< std::vector, char > ;
         small_vector v(4, '*');
         // v.resize(4);
 
@@ -38,15 +16,14 @@ static auto dumsy = []
         auto data_ = v.data();
         auto size_ = v.size();
 
-        return true;
-}();
+});
 
 #include <intrin.h>
 
 template <typename T, std::size_t Alignment>
-using aaloc = allignedalloc_space::aligned_allocator<T, Alignment>;
+using aaloc = dbj::nanolib::alloc::aligned_allocator<T, Alignment>;
 
-static auto test_aligned_allocator = []()
+TU_REGISTER([]()
 {
     typedef std::vector<__m128, aaloc<__m128, sizeof(__m128)> > aligned_vector;
 
@@ -74,5 +51,4 @@ static auto test_aligned_allocator = []()
 
     __m128 mul = _mm_mul_ps(lhs[10], rhs[10]);
 
-    return mul;
-}();
+});
